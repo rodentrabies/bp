1e33254421663bf7fd311c46f0a4e19c66086575
whythat <whythat@protonmail.com>
2019-07-29 00:31:06 +0300

    crypto: add SHA1 function

--- crypto/hash.lisp
+++ crypto/hash.lisp
@@ -1,10 +1,18 @@
 (uiop:define-package :bp/crypto/hash
     (:use :cl :ironclad)
   (:export
+   #:sha1
+   #:ripemd160
+   #:sha256
    #:hash256
    #:hash160))
 
 (in-package :bp/crypto/hash)
+
+(defun sha1 (bytes)
+  (let ((digester (ironclad:make-digest 'ironclad:sha1)))
+    (ironclad:update-digest digester bytes)
+    (ironclad:produce-digest digester)))
 
 (defun sha256 (bytes)
   (let ((digester (ironclad:make-digest 'ironclad:sha256)))
