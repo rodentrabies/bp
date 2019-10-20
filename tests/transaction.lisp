e1e7231b41f9534ee4c8abed885d8df5481190cf
whythat <whythat@protonmail.com>
2019-10-20 17:34:00 +0300

    tests: add validation test for common transaction types
    These transaction types are tested: p2pk, p2pkh, p2ms, p2sh,
    p2sh-p2wpkh, p2sh-p2wsh, p2wpkh, p2wsh. Examples are taken from
    mainnet.

--- tests/transaction.lisp
+++ tests/transaction.lisp
@@ -13,7 +13,24 @@ component extraction/computation.")
 (test parsing/serialization-isomorphism
   :description "Test that the serialization of the parsed hex
 transaction blob is the same as the original blob."
-  (loop
-     :for txid  :in *all-test-transactions*
-     :for txhex := (chain-get-transaction :test-chain-supplier txid)
-     :do (is (equal (encode (decode 'tx txhex)) txhex))))
+  (with-chain-supplier (test-chain-supplier)
+    (loop
+       :for txid  :in *all-test-transactions*
+       :for txhex := (get-transaction txid :encoded t)
+       :do (is (equal (encode (decode 'tx txhex)) txhex)))))
+
+(test simple-validation
+  :description "Test that the simple validation works for common
+transaction types."
+  (with-chain-supplier (test-chain-supplier)
+    (let ((txs (list *p2pk-tx*
+                     *p2ms-tx*
+                     *p2pkh-tx*
+                     *p2sh-tx*
+                     *p2sh-p2wpkh-tx*
+                     *p2sh-p2wsh-tx*
+                     *p2wpkh-tx*
+                     *p2wsh-tx*)))
+      (loop
+         :for txid :in txs
+         :do (is (validp (get-transaction txid)))))))
