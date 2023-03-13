08bcb44d412fb0538cec081c145a671814ad5e9d
Oscar Najera <hi@oscarnajera.com>
2023-03-13 21:38:37 +0100

    crypto: expose secp256k1 `combine-pubkeys`

--- tests/all.lisp
+++ tests/all.lisp
@@ -1,6 +1,7 @@
 (uiop:define-package :bp/tests/all (:use :cl)
   (:use
    :bp/tests/encoding
+   :bp/tests/crypto
    :bp/tests/block
    :bp/tests/transaction
    :bp/tests/script))
---
+++ tests/crypto.lisp
@@ -0,0 +1,32 @@
+(uiop:define-package :bp/tests/crypto (:use :cl :fiveam)
+  (:use :bp/crypto/all))
+
+(in-package :bp/tests/crypto)
+
+(def-suite crypto-tests
+  :description "Tests for crypto tools.")
+
+(in-suite crypto-tests)
+
+(defun scale-key (scalar seckey)
+  (bp/crypto/secp256k1::%make-key
+   :bytes
+   (ironclad:integer-to-octets
+    (mod
+     (* scalar (ironclad:octets-to-integer (bp/crypto/secp256k1::key-bytes seckey)))
+     #xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141)
+    :n-bits #.(* 8 32))))
+
+(test pubkey-combination
+  (for-all ((scalar (gen-integer :min 1 :max 10)))
+    (let* ((seckey (make-key))
+           (pubkey (make-pubkey seckey))
+           (combined pubkey)
+           (scaled-sec (scale-key scalar seckey))
+           (scaled-pub (make-pubkey scaled-sec)))
+      (dotimes (_ (1- scalar))
+        (setq combined (combine-pubkeys combined pubkey)))
+      (is (equalp scaled-pub combined))
+      ;; using reduce vector
+      (is (equalp scaled-pub
+                  (apply #'combine-pubkeys (make-list scalar :initial-element pubkey)))))))
